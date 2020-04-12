907c59747c2d3b52235ceee429809ca87cc530a1
Marius <stoicamarius3301@gmail.com>
2020-04-13 00:26:43 +0300

    PizzaDough model created

---
+++ DesignPatternsProject/DesignPatternsProject/model/PizzaDough.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DesignPatternsProject.utils;
+
+namespace DesignPatternsProject.model
+{
+    class PizzaDough
+    {
+        public EDoughType doughType { get; set; }
+
+        public int price { get; set; }
+
+        public PizzaDough(EDoughType type, int price)
+        {
+            doughType = type;
+            this.price = price;
+        }
+    }
+}
