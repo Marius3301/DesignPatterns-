26213a1dc1f9aadc5e8707917208cd45549e20e2
Marius <stoicamarius3301@gmail.com>
2020-04-13 00:26:13 +0300

    Constants and EDoughType created

---
+++ DesignPatternsProject/DesignPatternsProject/utils/Constans.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsProject.utils
+{
+    class Constans
+    {
+       public  const int CRUSTY_DOUGH_PRICE = 8;
+       public  const int NORMAL_DOUGH_PRICE = 10;
+       public  const int CHEESY_DOUGH_PRICE = 15;
+    }
+}
---
+++ DesignPatternsProject/DesignPatternsProject/utils/EDoughType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsProject.utils
+{
+    enum EDoughType
+    {
+        ENormal,
+        ECrusty,
+        ECheesy
+    }
+}
